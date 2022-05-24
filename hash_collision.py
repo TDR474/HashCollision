a1b76f765bcfcf5f94b4c07ae993348b5d5046a8
frankzhuao123 <91228378+frankzhuao123@users.noreply.github.com>
2022-05-23 20:51:40 -0400

    Update hash_collision.py

--- hash_collision.py
+++ hash_collision.py
@@ -12,14 +12,31 @@ def hash_collision(k):
         return( b'\x00',b'\x00' )
    
     #Collision finding code goes here
-    # SHA256(x) and SHA256(y) shoul match on their final k bits 
-    # x and y are the two strings to be hashed
+    # x and y are two randomly generated variables
+    # SHA256(x) and SHA256(y) should match on their final k bits 
     # k is the number of bits to be matched
     # The return variables, x and y, should be encoded as bytes.
     # iterate over 2^k possibilities of x and y
-    for x in range(2**k):
-        x_hash = hashlib.sha256(str(x).encode('utf-8')).hexdigest()
-        for y in range(2**k):
-            y_hash = hashlib.sha256(str(y).encode('utf-8')).hexdigest()
-            if x_hash[-k:] == y_hash[-k:]:
-                return(str(x).encode('utf-8'),str(y).encode('utf-8'))
+
+    # randomly generate a three-letter string (NOT bytes) x
+    # randomly generate a three-letter string (NOT bytes) y
+    # hash x and y using SHA256
+    # check if the final k bits of the hash of x and y match
+    # loop until the final k bits match
+    # return x and y
+    x = os.urandom(3)
+    y = os.urandom(3)
+    x_hash = hashlib.sha256(x).digest()
+    y_hash = hashlib.sha256(y).digest()
+    x_hash_final = x_hash[-k:]
+    y_hash_final = y_hash[-k:]
+    while x_hash_final != y_hash_final:
+        x = os.urandom(3)
+        y = os.urandom(3)
+        x_hash = hashlib.sha256(x).digest()
+        y_hash = hashlib.sha256(y).digest()
+        x_hash_final = x_hash[-k:]
+        y_hash_final = y_hash[-k:]
+    return(x,y)
+
+
