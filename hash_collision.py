f53cf2517a7daad78a8535d84043e8649df157fb
Frank Zhu <aoz@usc.edu>
2022-05-24 00:04:17 +0000

    blah

--- hash_collision.py
+++ hash_collision.py
@@ -1,3 +1,5 @@
+# Write a brute-force algorithm to find a partial collision 
+
 import hashlib
 import os
 
@@ -10,11 +12,14 @@ def hash_collision(k):
         return( b'\x00',b'\x00' )
    
     #Collision finding code goes here
-    x = b'\x00'
-    y = b'\x00'
-    
-    return( x, y )
-
-
-
-
+    # SHA256(x) and SHA256(y) shoul match on their final k bits 
+    # x and y are the two strings to be hashed
+    # k is the number of bits to be matched
+    # The return variables, x and y, should be encoded as bytes.
+    # iterate over 2^k possibilities of x and y
+    for x in range(2**k):
+        x_hash = hashlib.sha256(str(x).encode('utf-8')).hexdigest()
+        for y in range(2**k):
+            y_hash = hashlib.sha256(str(y).encode('utf-8')).hexdigest()
+            if x_hash[-k:] == y_hash[-k:]:
+                return(str(x).encode('utf-8'),str(y).encode('utf-8'))
